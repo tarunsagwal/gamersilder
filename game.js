94f4b61cbba48bb3f9a4f86464f25332b22bf0ad
Tarun <tarunsagwal38@gmail.com>
2024-02-15 13:19:34 +0530

    service page comnplete

--- game.js
+++ game.js
@@ -3,14 +3,31 @@ let div=document.createElement("div");
 body.appendChild(div)
 div.setAttribute("class","loader");
 div.setAttribute("onload","loader()");
-let cricle=document.createElement("div");
-div.appendChild(cricle)
-cricle.setAttribute("class","cricle");
 let type=document.createElement("p");
 div.appendChild(type);
 type.setAttribute("class","type");
 var app = document.querySelector(".type");
+let cricle=document.createElement("i");
+div.appendChild(cricle)
+cricle.setAttribute("class","bx bxl-spring-boot");
+cricle.setAttribute("style","color:red;");
 
+window.addEventListener("load", (event) => {
+    timer= setInterval(()=>{
+        let divi=document.querySelector(".loader")
+        divi.classList.remove("loader")
+        cricle.classList.remove("bxl-spring-boot")
+        type.classList.remove("type");
+        type.remove();
+        if(timer<1){
+            divi.classList.add("loaders")
+            cricle.classList.add("bxl-spring-boots")
+            type.classList.add("types")
+        }
+    },4000)
+    console.log(timer);
+
+  });
 var typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
@@ -19,16 +36,16 @@ var typewriter = new Typewriter(app, {
 typewriter
 //   .pauseFor(250)
   .typeString('welcome back')
-  .pauseFor(300)
+  .pauseFor(100)
   .deleteChars(4)
   .typeString('<strong><span style="color:red">suko game</span></strong>')
   .pauseFor(2500)
-  .deleteChars(4)
-  .typeString('give you anthor world')
-  .pauseFor(200)
-  .deleteChars(26)
-  .typeString('more fun')
-  .pauseFor(1000)
+//   .deleteChars(4)
+//   .typeString('give you anthor world')
+//   .pauseFor(200)
+//   .deleteChars(26)
+//   .typeString('more fun')
+  .pauseFor(10000)
   .start();
 console.log(div)
 function transfrom(){
@@ -57,22 +74,6 @@ function nontrans(){
 home.classList.remove("tapchange")
 
 }
-window.addEventListener("load", (event) => {
-    timer= setInterval(()=>{
-        let divi=document.querySelector(".loader")
-        divi.classList.remove("loader")
-        cricle.classList.remove("cricle")
-        type.classList.remove("type");
-        type.remove();
-        if(timer<1){
-            divi.classList.add("loaders")
-            cricle.classList.add("cricles")
-            type.classList.add("types")
-        }
-    },5000)
-    console.log(timer);
-
-  });
 let icon=document.querySelector(".icon");
 let sun=document.querySelector("#sun");
 let moon=document.querySelector("#moon");
